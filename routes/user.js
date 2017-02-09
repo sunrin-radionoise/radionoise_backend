a90b865a001ae9a7fe3e3bcae9f6a90e8a1adcfe
iwin2471 <iwin12372@gmail.com>
2017-02-09 22:13:43 +0900

    [facebook] add facebook peristalsis

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-module.exports = (router, Users) => {
+module.exports = (router, Users, passport) => {
   router.get('/user', (req, res) =>{
     Users.find({}, (err, user)=> {
       if(err) res.status(400).send("db err");
@@ -31,6 +31,20 @@ module.exports = (router, Users) => {
     }else res.status(401).send("unauthed");
   })
 
+  .get('/user/:token/fb/token', passport.authenticate('facebook-token'), (req, res)=>{
+    if (req.user) {
+      Users.findOne({facebook_id: req.user._json.id}, function(err, users) {
+        if(err) err;
+        if(users) return res.status(409).send("already exist");
+        else{
+          Users.update({token: req.params.token}, {$set: {facebook_id: req.user._json.id}}, (err, result)=>{
+            if(err) return res.stauts(500).send("DB ERR");
+            if(result) return res.status(200).send("ok");
+          });
+        }
+      });
+    }else res.status(401).send("unauthed");
+  })
 
   return router;
 }
