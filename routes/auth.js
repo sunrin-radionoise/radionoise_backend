29b4940b9b5840b0155e0a0ac7f7f7db6902112b
iwin2471 <iwin12372@gmail.com>
2017-02-09 23:22:45 +0900

    [twitter] add twitter peristalsis

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 module.exports = (router, rnd_string, Users, passport, func) =>{
-  router.post('/auth/signup', (req, res) => {
+  router.post('/auth/signin', (req, res) => {
     var params = ['id', 'passwd', 'name'];
 
     if(func.check_param(req.body, params)){
@@ -24,7 +24,7 @@ module.exports = (router, rnd_string, Users, passport, func) =>{
     } 
   })
   
-  .post('/auth/signin', (req,res)=>{
+  .post('/auth/signup', (req,res)=>{
     var params = ['id', 'passwd'];
     if(func.check_param(req.body, params)){
       Users.findOne({id: req.body.id, passwd: req.body.passwd}, (err, user)=>{
--- routes/user.js
+++ routes/user.js
@@ -45,6 +45,21 @@ module.exports = (router, Users, passport) => {
       });
     }else res.status(401).send("unauthed");
   })
+  
+  .get('/user/:token/tw/token', passport.authenticate('twitter-token'), (req, res)=>{
+    if (req.user) {
+      Users.findOne({twitter_id: req.user._json.id}, function(err, users) {
+        if(err) err;
+        if(users) return res.status(409).send("already exist");
+        else{
+          Users.update({token: req.params.token}, {$set: {twitter_id: req.user._json.id}}, (err, result)=>{
+            if(err) return res.stauts(500).send("DB ERR");
+            if(result) return res.status(200).send("ok");
+          });
+        }
+      });
+    }else res.status(401).send("unauthed");
+  })
 
   return router;
 }
