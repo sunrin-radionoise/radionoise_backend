585ef9b772df11964942d6db075b31c192b59b84
iwin2471 <iwin12372@gmail.com>
2017-02-13 18:39:30 +0900

    [socket] add join

--- socket.js
+++ socket.js
@@ -4,27 +4,32 @@ module.exports = (io, Users, rndString, Chats) =>{
       io.broadcast.to(room_token).emit('message', msg);
     })
 
-   .on('join', (room_token, token)=>{
-     Chats.findOne({room_token: room_token}, (err, user)=> {
-       if(err) throw err;
-       if(users)  socket.room.join(user.token);
-     });
-   })
+    .on('make', (token, chat_people)=>{
+      var new_Chat = new Chats({
+        chat_people: chat_people,
+        room_token: rndString.generate(),
+      });
 
-  .on('make', (token, chat_people)=>{
-    var new_Chat = new Chats({
-      chat_people: chat_people,
-      room_token: rndString.generate(),
-    });
+      new_Chat.save((err, result)=>{
+        if(err) io.emit('make', 'save err');
+        if(result){
+          io.emit('make', new_Chat.room_token);
+          io.room.join(new_Chat.room_token);
+        }
+      });
+    })
 
-    new_Chat.save((err, result)=>{
-      if(err) io.emit('make', 'save err');
-      if(result){
-        io.emit('make', new_Chat.room_token);
-        io.room.join(new_Chat.room_token);
-      }
-    });
-  })
+    .on('join', (room_token, token)=>{
+       Chats.findOne({room_token: room_token}, (err, user)=> {
+         if(err) throw err;
+         if(users)  socket.room.join(user.token);
+       });
+    })
+
+    .on('message', (msg)=>{
+      io.broadcast.emit('message', msg);
+    })
+   
 
   }); 
 }
